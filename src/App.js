20aafb50aced153cc7c549ebd8e195c18ec328d3
Abdulrehman Ahmad <abdulrehman133782@gmail.com>
2023-05-05 17:41:26 +1000

    game no longer continues once winner is found

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function Board() {
   function handleClick(i) {
     const newSquares = squares.slice();
 
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -48,4 +48,24 @@ export default function Board() {
       </div>
     </Fragment>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
